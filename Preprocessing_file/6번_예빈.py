fde05d9a5b9028bd2e49df88418876b8cc37b02f
Lee Yebin <stella_yb@handong.ac.kr>
2025-04-27 22:48:27 +0900

    6번_예빈.py

--- Preprocessing_file/6번_예빈.py
+++ Preprocessing_file/6번_예빈.py
@@ -5,6 +5,24 @@ from scipy import stats
 # CSV 파일 불러오기
 df = pd.read_csv("Dataset/6번_StudentsPerformance.csv")
 
+#  문자열 공백 제거 + 결측치 'Unknown'으로 대체
+df['parental level of education'] = df['parental level of education'].str.strip()
+df['parental level of education'] = df['parental level of education'].fillna('Unknown')
+
+#  재분류 맵 만들기
+edu_map = {
+    'some high school': 'High School',
+    'high school': 'High School',
+    'some college': 'College',
+    "associate's degree": 'College',
+    "bachelor's degree": 'University',
+    "master's degree": 'University',
+    "Unknown": 'Unknown'  # 결측치 대체용
+}
+
+#  그룹핑
+df['parental_edu_group'] = df['parental level of education'].map(edu_map)
+
 # 결측치 확인
 print("결측치 확인:\n", df.isnull().sum())
 
@@ -14,7 +32,6 @@ z_scores = stats.zscore(df[score_cols])
 
 # Z-score 절댓값이 3 미만인 행만 남김
 df = df[(np.abs(z_scores) < 3).all(axis=1)]
-print(f"\n이상치 제거 후 데이터 수: {len(df)}")
 
 # 범주형 변수 → pandas.get_dummies()로 인코딩
 df_dummies = pd.get_dummies(df, columns=[
